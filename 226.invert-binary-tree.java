72289102b9bc52a016da04febfd99bd9c80e1e90
Ritik Garg <gargritik5@gmail.com>
2024-02-13 18:46:08 +0530

    226. Invert Binary Tree

---
+++ 226.invert-binary-tree.java
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode id=226 lang=java
+ *
+ * [226] Invert Binary Tree
+ */
+
+/*
+ * 77/77 cases passed (0 ms)
+ * Your runtime beats 100 % of java submissions
+ * Your memory usage beats 27.07 % of java submissions (41.3 MB)
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null) return null;
+        invertTree(root.left);
+        invertTree(root.right);
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        return root;
+    }
+}
+// @lc code=end
+
